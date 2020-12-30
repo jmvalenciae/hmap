5fd834ca5cfe3fd7e83f64b2b88674bbb2d29241
jvech <juanma9901@gmail.com>
2020-12-30 18:28:44 -0500

    I Give up with Geoportal icons for now
    Remember that the other name of the icon is imagepath2.png with
    imagepath being the original name

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -24,42 +24,50 @@
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
                                 <a class="nav-link" href="http://cdiac.manizales.unal.edu.co/sistema-alerta-temprana-test/MapaManizales/">
-                                    <img onmouseover="this.src='../assets/images/inicio2.svg';" onmouseout="this.src='../assets/images/inicio.svg';" src="../assets/images/inicio.svg" class="img-fluid" width="35" height="35" alt="Logo del Sitio" title="Inicio">
+                                    <img src="../assets/images/inicio.svg" 
+                                         class="img-fluid" width="35" height="35" alt="Logo del Sitio" title="Inicio">
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="http://idea.manizales.unal.edu.co/reporte-meteorologico.html" target="_blank" title="Otros Reportes">
-                                    <img onmouseover="this.src='../assets/images/meteorologico2.svg';" onmouseout="this.src='../assets/images/meteorologico.svg';" src="../assets/images/meteorologico.svg" width="35" height="35" alt="Logo del Sitio" title="Reporte Meteorológico">
+                                    <img src="../assets/images/meteorologico.svg" 
+                                         width="35" height="35" alt="Logo del Sitio" title="Reporte Meteorológico">
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="http://cdiac.manizales.unal.edu.co/gestion-riesgo/web/app.php/gestion_riesgo/generar_reporte/seleccionar_tipo_reporte" target="_blank" title="Reportes">
-                                    <img onmouseover="this.src='../assets/images/generaReporte2.svg';" onmouseout="this.src='../assets/images/generaReporte.svg';" src="../assets/images/generaReporte.svg" width="35" height="35" alt="Logo del Sitio" title="Reportes">
+                                    <img src="../assets/images/generaReporte.svg" 
+                                         width="35" height="35" alt="Logo del Sitio" title="Reportes">
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a  class="nav-link" href="http://cdiac.manizales.unal.edu.co/gestion-riesgo/web/app.php/gestion_riesgo/generar_reporte/seleccionar_tipo_reporte_grafico" target="_blank" title="Reportes Gráficos">
-                                    <img onmouseover="this.src='../assets/images/reporteGrafico2.svg';" onmouseout="this.src='../assets/images/reporteGrafico.svg';" src="../assets/images/reporteGrafico.svg" width="35" height="35" alt="Logo del Sitio" title="Reportes Gráficos">
+                                    <img src="../assets/images/reporteGrafico.svg" 
+                                         width="35" height="35" alt="Logo del Sitio" title="Reportes Gráficos">
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="http://cdiac.manizales.unal.edu.co/indicadores/public/index" target="_blank" >
-                                    <img onmouseover="this.src='../assets/images/cdiac2.svg';" onmouseout="this.src='../assets/images/cdiac.svg';" src="../assets/images/cdiac.svg" width="35" height="35" title="Datos Históricos">
+                                    <img src="../assets/images/cdiac.svg" 
+                                         width="35" height="35" title="Datos Históricos">
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="https://www.facebook.com/SIMACUNAL/" target="_blank" >
-                                    <img onmouseover="this.src='../assets/images/facebook2.svg';" onmouseout="this.src='../assets/images/facebook.svg';" src="../assets/images/facebook.svg" width="35" height="35" alt="Logo del Sitio" title="Página de Facebook">
+                                    <img src="../assets/images/facebook.svg" 
+                                         width="35" height="35" alt="Logo del Sitio" title="Página de Facebook">
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="https://twitter.com/simac_unal" target="_blank">
-                                    <img onmouseover="this.src='../assets/images/twitter2.svg';" onmouseout="this.src='../assets/images/twitter.svg';" src="../assets/images/twitter.svg" width="35" height="35" alt="Logo del Sitio" title="Página de Twitter">
+                                    <img src="../assets/images/twitter.svg" 
+                                         width="35" height="35" alt="Logo del Sitio" title="Página de Twitter">
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="http://cdiac.manizales.unal.edu.co/gestion-riesgo/web/gestion_riesgo/solicitud_informacion/seleccionar_formulario" target="_blank">
-                                    <img onmouseover="this.src='../assets/images/correo2.svg';" onmouseout="this.src='../assets/images/correo.svg';" src="../assets/images/correo.svg" width="35" height="35" alt="Logo del Sitio" title="Contáctanos">
+                                    <img src="../assets/images/correo.svg" 
+                                         width="35" height="35" alt="Logo del Sitio" title="Contáctanos">
                                 </a>
                             </li>
                         </ul>
@@ -120,6 +128,12 @@
 <script>
 export default {
     name: 'Index',
+    methods:{
+        mouseOver: function(imagepath){
+            this.src = '../assets/images/inicio2.svg';
+            return imagepath;
+        }
+    }
 }
 </script>
 
