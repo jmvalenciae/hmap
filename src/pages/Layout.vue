117e24a1e773b733af7fb563be50168f00615fb3
jvech <jmvalenciae@unal.edu.co>
2021-01-28 21:06:18 -0500

    Restructuring the project
    I expect not much work please :(

---
+++ src/pages/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- .container -->
+  <div class="container-fluid">
+      <!-- /.row -->
+
+      <div class="row">
+      <div class="col-12">
+          <HMap/>
+          <Index/>
+      </div>
+      </div>
+      <!-- /.row -->
+  </div>
+  <!-- /.container -->
+</template>
+
+<script>
+import HMap from './HMap.vue';
+import Index from './Index.vue'
+export default {
+  name: 'Layout',
+  components: {HMap, 
+               Index}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/routes.js
@@ -0,0 +1,36 @@
+
+/*
+|-------------------------------------------------------------------------------
+| routes.js
+|-------------------------------------------------------------------------------
+| Contains all of the routes for the application
+*/
+
+/*
+    Imports Vue and VueRouter to extend with the routes.
+*/
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+/*
+    Extends Vue to use Vue Router
+*/
+Vue.use( VueRouter );
+
+/*
+    Makes a new VueRouter that we will use to run all of the routes
+    for the app.
+*/
+/*
+ */
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            name: 'layout',
+            component: Vue.component('Layout', require ('./pages/Layout.vue').default),
+            children: [
+            ]
+        }
+    ]
+});
