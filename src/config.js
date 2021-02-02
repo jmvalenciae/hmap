81ce78374235777384284796a7787fc556c7634b
jvech <jmvalenciae@unal.edu.co>
2021-02-02 07:53:14 -0500

    config.js add Know i have to figure out how to use it

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const icons = '@/assets/icons/'
+const images = '@/assets/images/'
+const json_url = '@/assets/json/'
+
+export const ALERT_SYSTEM_CONFIG = {
+    IMG_URL: images,
+    ICON_URL: icons,
+    JSON_URL: json_url
+}
