39b45baaaba27a590905fe7cd04c82d2e8bde65d
jvech <jmvalenciae@unal.edu.co>
2021-02-11 08:10:42 -0500

    The API will be later
    Aparently the fetch works when the data is loaded in the sever

--- src/api/stations.js
+++ src/api/stations.js
@@ -1,9 +1,13 @@
-import {ALERT_SYSTEM_CONFIG} from '../config.js'
+//import {ALERT_SYSTEM_CONFIG} from '../config.js'
 
-import stations from '../assets/json/estaciones.json'
+import stations from '@/assets/json/estaciones.json'
 
+//let myRequest = new Request(ALERT_SYSTEM_CONFIG.JSON_URL+'estaciones.json')
 export default {
     getStations: function(){
-        return stations;
+        return stations
+        //return fetch(myRequest)
+        //       .then(response => response.text())
+        //       .then(obj => console.log(obj));
     }
 }
